9e0de7da9d45a2801a07c59781903190dd2d838d
Hoodoo42 <create@hoodooverse.ca>
2022-08-13 10:29:13 -0600

    added a few more steps to get selected item based on price. Need to add if statement

--- src/components/MenuItems.vue
+++ src/components/MenuItems.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <!-- this will filter the users price selection showing only items with prices that are equal too or less than the number they enterd -->
-    <input type="number" />
+    <input type="number" ref="filter_price">
     <button @click="price_filter">Price Filter</button>
     <!-- using a for loop inside HTML to display the items in the array inside the data js section -->
     <article v-for="(item, index) in items" :key="index">
+        <div>
       <h3>{{ item[`title`] }}</h3>
       <p>{{ item[`price`] }}</p>
       <h4>{{ item[`description`] }}</h4>
+      </div>
     </article>
   </div>
 </template>
@@ -15,11 +17,15 @@
 <script>
 export default {
   methods: {
-    price_filter() {},
+    price_filter() {
+        this.max_price = this.$refs[`filter_price`][`value`];
+    },
   },
 
   data() {
     return {
+        max_price: 100,
+
       items: [
         {
           title: `Fruit Cup`,
